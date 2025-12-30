4b0ea305f80696e46f578ee640f9a6e3649adcd8
auwaldev-creator <auwalbashar12@gmail.com>
2025-12-30 12:57:02 +0100

    Create supabase-config.js with configuration
    Added Supabase configuration with URL and anon key.

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// supabase-config.js
+// SAKA KEY DINKA A NAN (Na cire naka saboda tsaro)
+const SUPABASE_URL = 'https://emsfczgikwhfjpgwkyqv.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_AuyN3-6VqWkMn11d1T_wmg_-RagFA47'; // Saka cikakken key dinka anan
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
